c8f5b4cb7079579c7b8f9ebca954d69dd9484b63
Bernard Van De Walle <bernard@bvandewa.com>
2016-09-08 16:36:06 -0700

    removing flags

--- kubernetes.go
+++ kubernetes.go
@@ -5,7 +5,6 @@ package main
 // This is based on the original example code from trireme/example/example.go
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"sync"
@@ -14,6 +13,7 @@ import (
 	"github.com/aporeto-inc/trireme"
 	"github.com/aporeto-inc/trireme/datapath"
 	"github.com/aporeto-inc/trireme/policy"
+
 	"github.com/docker/engine-api/types"
 	"k8s.io/kubernetes/pkg/api"
 	apiu "k8s.io/kubernetes/pkg/api/unversioned"
@@ -137,19 +137,6 @@ func (k *KubernetesPolicy) MetadataExtractor(info *types.ContainerJSON) (string,
 	}
 
 	return contextID, container, nil
-}
-
-func usage() {
-	fmt.Fprintf(os.Stderr, "usage: example -stderrthreshold=[INFO|WARN|FATAL] -log_dir=[string]\n")
-	flag.PrintDefaults()
-	os.Exit(2)
-}
-
-func init() {
-	flag.Usage = usage
-	// NOTE: This next line is key you have to call flag.Parse() for the command line
-	// options or "flags" that are defined in the glog module to be picked up.
-	flag.Parse()
 }
 
 func main() {
