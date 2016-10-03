1e077f439d407cce25cf576a8fed6913e4636a35
Bernard Van De Walle <bernard@bvandewa.com>
2016-10-03 13:44:15 -0700

    Complete Selector

--- resolver/selector.go
+++ resolver/selector.go
@@ -1,6 +1,8 @@
 package resolver
 
 import (
+	"fmt"
+
 	"github.com/aporeto-inc/trireme/policy"
 	apiu "k8s.io/kubernetes/pkg/api/unversioned"
 	"k8s.io/kubernetes/pkg/apis/extensions"
@@ -8,37 +10,47 @@ import (
 	"k8s.io/kubernetes/pkg/selection"
 )
 
-func clauseEqual(requirement labels.Requirement) []policy.KeyValueOperator {
+func clauseEquals(requirement labels.Requirement) []policy.KeyValueOperator {
 	kvo := policy.KeyValueOperator{
 		Key:      requirement.Key(),
 		Operator: policy.Equal,
-		Value:    requirement.Values().List()[0],
+		Value:    requirement.Values().List(),
+	}
+	return []policy.KeyValueOperator{kvo}
+}
+
+func clauseNotEquals(requirement labels.Requirement) []policy.KeyValueOperator {
+	kvo := policy.KeyValueOperator{
+		Key:      requirement.Key(),
+		Operator: policy.NotEqual,
+		Value:    requirement.Values().List(),
 	}
 	return []policy.KeyValueOperator{kvo}
 }
 
 func clauseIn(requirement labels.Requirement) []policy.KeyValueOperator {
-	return nil
+	kvo := policy.KeyValueOperator{
+		Key:      requirement.Key(),
+		Operator: policy.Equal,
+		Value:    requirement.Values().List(),
+	}
+	return []policy.KeyValueOperator{kvo}
 }
 
 func clauseNotIn(requirement labels.Requirement) []policy.KeyValueOperator {
-	kvos := []policy.KeyValueOperator{}
-	for _, value := range requirement.Values().List() {
-		kvo := policy.KeyValueOperator{
-			Key:      requirement.Key(),
-			Operator: policy.NotEqual,
-			Value:    value,
-		}
-		kvos = append(kvos, kvo)
+	kvo := policy.KeyValueOperator{
+		Key:      requirement.Key(),
+		Operator: policy.NotEqual,
+		Value:    requirement.Values().List(),
 	}
-	return kvos
+	return []policy.KeyValueOperator{kvo}
 }
 
 func clauseExists(requirement labels.Requirement) []policy.KeyValueOperator {
 	kvo := policy.KeyValueOperator{
 		Key:      requirement.Key(),
-		Operator: policy.Equal,
-		Value:    "*",
+		Operator: policy.ANY,
+		Value:    []string{"*"},
 	}
 	return []policy.KeyValueOperator{kvo}
 }
@@ -46,19 +58,33 @@ func clauseExists(requirement labels.Requirement) []policy.KeyValueOperator {
 func clauseDoesNotExist(requirement labels.Requirement) []policy.KeyValueOperator {
 	kvo := policy.KeyValueOperator{
 		Key:      requirement.Key(),
-		Operator: policy.NotEqual,
-		Value:    "*",
+		Operator: policy.ANY,
+		Value:    []string{"*"},
 	}
 	return []policy.KeyValueOperator{kvo}
 }
 
-// generatePortTags generates all the
+// generatePortTags generates all the clauses for the ports
 func generatePortTags(ports []extensions.NetworkPolicyPort) []policy.KeyValueOperator {
-	// If no Ports are explicite
-	if len(ports) == 0 {
+	// If Port is not defined, then no need for specific traffic matching.
+	if ports == nil {
 		return []policy.KeyValueOperator{}
 	}
-	return nil
+	// If Port is defined but no ports are defined into it, then No traffic is matched at all
+	if len(ports) == 0 {
+		return nil
+	}
+
+	portList := []string{}
+	for _, port := range ports {
+		portList = append(portList, port.Port.String())
+	}
+	kvo := policy.KeyValueOperator{
+		Key:      "@port",
+		Operator: policy.ANY,
+		Value:    portList,
+	}
+	return []policy.KeyValueOperator{kvo}
 
 }
 
@@ -73,13 +99,19 @@ func individualRule(req *policy.ContainerInfo, rule *extensions.NetworkPolicyIng
 		}
 
 		requirements, _ := peerSelector.Requirements()
+
+		// Initialize the completeClause with the port matching
 		completeClause := []policy.KeyValueOperator{}
+		completeClause = append(completeClause, generatePortTags(rule.Ports)...)
 		for _, requirement := range requirements {
 
 			// Each requirement is ANDed
 			switch requirement.Operator() {
 			case selection.Equals:
-				requirementClause := clauseEqual(requirement)
+				requirementClause := clauseEquals(requirement)
+				completeClause = append(completeClause, requirementClause...)
+			case selection.NotEquals:
+				requirementClause := clauseNotEquals(requirement)
 				completeClause = append(completeClause, requirementClause...)
 			case selection.In:
 				requirementClause := clauseIn(requirement)
@@ -104,4 +136,12 @@ func individualRule(req *policy.ContainerInfo, rule *extensions.NetworkPolicyIng
 	}
 
 	return nil
+}
+
+func printRules(req *policy.ContainerInfo) {
+	for _, selector := range req.Policy.Rules {
+		for _, clause := range selector.Clause {
+			fmt.Printf("%+v\n", clause)
+		}
+	}
 }
