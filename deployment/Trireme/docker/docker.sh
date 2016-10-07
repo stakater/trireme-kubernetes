aa052a8ffed445142e72894f5667f421b02e5474
Bernard Van De Walle <bernard@bvandewa.com>
2016-10-07 11:32:03 -0700

    Changes to Docker run

--- deployment/Trireme/docker/docker.sh
+++ deployment/Trireme/docker/docker.sh
@@ -4,6 +4,6 @@ docker run \
   --net host \
   -t \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /root/.kube/config:/root/.kube/config
-  --restart always
+  -v /root/.kube/config:/root/.kube/config \
+  --restart always \
 926088932149.dkr.ecr.us-west-2.amazonaws.com/kubernetes-integration
