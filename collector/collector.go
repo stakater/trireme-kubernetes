842dc41945f3c9c1b41e25f4b1d356df8cee0ffb
Bernard Van De Walle <bernard@bvandewa.com>
2017-10-16 14:17:51 -0700

    Adapt to new influxdb (#101)
    * New: Using the new BufferEnabled collector
    
    * Calling NewDB
    
    * Renaming NewDB to NewDBConnection
    
    * Fix: Change of location for InfluxDB package

--- collector/collector.go
+++ collector/collector.go
@@ -1,7 +1,7 @@
 package collector
 
 import (
-	"github.com/aporeto-inc/trireme-statistics/collector/influxdb"
+	"github.com/aporeto-inc/trireme-statistics/influxdb"
 	"github.com/aporeto-inc/trireme/collector"
 	"go.uber.org/zap"
 )
@@ -15,6 +15,10 @@ func NewDefaultCollector() collector.EventCollector {
 // NewInfluxDBCollector returns a collector implementation for InfluxDB
 func NewInfluxDBCollector(user, pass, url string) collector.EventCollector {
 	zap.L().Info("Using Influx collector", zap.String("endpoint", url), zap.String("user", user))
-	collectorInstance := influxdb.CreateAndConnectDB(user, pass, url)
+	collectorInstance, err := influxdb.NewDBConnection(user, pass, url)
+	if err != nil {
+		return nil
+	}
+	collectorInstance.Start()
 	return collectorInstance
 }
