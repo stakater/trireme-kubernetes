518f4345bc810d0023efdd3b98f4c10cba1d59c4
Bernard Van De Walle <bernard@bvandewa.com>
2017-10-16 17:03:02 -0700

    Fix: Working PSK Deployment files (#103)
    * Removing PKI deployment
    
    * correct config for daemonSetPSK
    
    * Version

--- version/version.go
+++ version/version.go
@@ -4,4 +4,4 @@ package version
 const VERSION = "0.11"
 
 // REVISION is the revision of Trireme-Kubernetes
-const REVISION = "3a0b74888475b6e934e22dff79e9732440dd7705"
+const REVISION = "f58fbf93353274370692ffd80d1e4b45de7fb0cb"
